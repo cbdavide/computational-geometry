e4f3f28ac6bfffa93af6e027156f7bb6e7cf2da7
David Castelblanco Benavides <cbdavides@gmail.com>
2018-08-15 19:00:45 -0500

    in_triangle function implemented.

--- convex_hull/triangles_ch.cpp
+++ convex_hull/triangles_ch.cpp
@@ -7,37 +7,58 @@
  * by some points a, b, c that belong to the set of points A.
  *
  * Complexity: O(n ^ 4)
+ *
+ * This algorithm doesn't ensure that the points are ordered in any way.
+ * The order of the CH points will be the same that the order of the
+ * input points
  */
 
- #include "../util.cpp"
+#include "../util.cpp"
 
- vector<point> convex_hull(vector<point> A) {
-   int n = A.size();
-   vector<point> CH;
-   bool non_extreme[n] = { 0 };
+bool in_triangle(point a, point b, point c, point p) {
+  bool r = to_left(a, b, p);
+  bool t = to_left(b, c, p);
+  bool z = to_left(c, a, p);
 
-   for(int i=0; i<n; i++) {
-     for(int j=i+1; j<n; j++) {
-       for(int k=j+1; k<n; k++) {
+  return (r == t) && (r == z);
+}
 
-         for(int l=0; l<n; l++) {
+vector<point> convex_hull(vector<point> A) {
+  int n = A.size();
+  vector<point> CH;
+  bool non_extreme[n] = { 0 };
+
+  for(int i=0; i<n; i++) {
+    for(int j=i+1; j<n; j++) {
+      for(int k=j+1; k<n; k++) {
+
+        for(int l=0; l<n; l++) {
           if(l == i || l == j || l == k)
-            continue;
+          continue;
 
           if(in_triangle(A[i], A[j], A[k], A[l]))
             non_extreme[l] = 1;
-         }
-       }
-     }
-   }
+        }
+      }
+    }
+  }
 
-   for(int i=0; i<n; i++) {
-     if(!non_extreme[i]) CH.push_back(i);
-   }
-   return CH;
- }
+  for(int i=0; i<n; i++)
+  if(!non_extreme[i]) CH.push_back(A[i]);
+
+  return CH;
+}
 
  int main() {
-   vector<point> A(10);
-   convex_hull(A);
- }
+  int n, x, y;
+  cin >> n;
+  vector<point> A(n);
+  for(int i=0; i<n; i++) {
+    cin >> x >> y;
+    A[i].x = x; A[i].y = y;
+  }
+  vector<point> CH = convex_hull(A);
+  for(int i=0; i<CH.size(); i++) {
+    cout << CH[i].x << ' ' << CH[i].y << endl;
+  }
+}
